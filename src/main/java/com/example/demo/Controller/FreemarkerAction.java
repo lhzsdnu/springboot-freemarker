321bc33607c24bd51686d8663b9f395e31dee05b
lhzsdnu <2383376138@qq.com>
2018-10-13 20:01:30 +0800

    更新示例

--- src/main/java/com/example/demo/Controller/FreemarkerAction.java
+++ src/main/java/com/example/demo/Controller/FreemarkerAction.java
@@ -5,7 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @RequestMapping(value = "/freemarker")
@@ -26,6 +26,23 @@ public class FreemarkerAction {
     public String toDemo(Map<String, Object> map) {
         log.info("====>>跳转freemarker页面");
         map.put("name", "jack");
+        map.put("success", true);
+        List goodsList=new ArrayList();
+        Map goods1=new HashMap();
+        goods1.put("name", "苹果");
+        goods1.put("price", 5.8);
+        Map goods2=new HashMap();
+        goods2.put("name", "香蕉");
+        goods2.put("price", 2.5);
+        Map goods3=new HashMap();
+        goods3.put("name", "橘子");
+        goods3.put("price", 3.2);
+        goodsList.add(goods1);
+        goodsList.add(goods2);
+        goodsList.add(goods3);
+        map.put("goodsList", goodsList);
+        map.put("today", new Date());
+        map.put("point", 102920122);
         return "test";
     }
 }
--- src/main/resources/templates/test.ftl
+++ src/main/resources/templates/test.ftl
@@ -6,5 +6,58 @@
 <body>
 <#--我只是一个注释，我不会有任何输出  -->
 ${name},你好。
+
+<#assign linkman="周先生">
+联系人：${linkman}
+
+<#assign info={"mobile":"13301231212",'address':'北京市昌平区王府街'} >
+电话：${info.mobile} 地址：${info.address}
+
+<#include "head.ftl">
+
+<#if success=true>
+  你已通过实名认证
+<#else>
+  你未通过实名认证
+</#if>
+
+----商品价格表----<br>
+<#list goodsList as goods>
+    ${goods_index+1} 商品名称： ${goods.name} 价格：${goods.price}<br>
+</#list>
+
+共  ${goodsList?size} 条记录<br>
+
+<#assign text="{'bank':'工商银行','account':'10101920201920212'}" />
+<#assign data=text?eval />
+开户行：${data.bank} 账号：${data.account} <br>
+
+当前日期：${today?date} <br>
+当前时间：${today?time} <br>
+当前日期+时间：${today?datetime} <br>
+日期格式化：  ${today?string("yyyy年MM月")}
+
+<br><br>
+累计积分：${point?c}<br>
+
+<#if aaa??>
+  aaa变量存在
+<#else>
+  aaa变量不存在
+</#if>
+<br>
+${aaa!'bbb'}
+<br>
+
+<#if name??>
+  name变量存在
+<#else>
+  name变量不存在
+</#if>
+<br>
+${name!'bbb'}
+
+
+
 </body>
 </html>
